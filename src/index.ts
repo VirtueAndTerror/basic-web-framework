a097ff5d7df5102912d02b59a92a4806c70a473b
VirtueAndTerror <50920185+VirtueAndTerror@users.noreply.github.com>
2022-01-27 14:01:04 -0500

    updated parcel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 9, name: 'new record', age: 0 });
+const user = User.buildUser({id: 1 });
 
 user.on('save', () => {
   console.log('User has changed, we probably need to update some HTML');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,57 +1,22 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 
 export interface UserProps {
-  id?: number;
-  name?: string;
-  age?: number;
+	id?: number;
+	name?: string;
+	age?: number;
 }
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  // Hardcoded instance of Eventing Class
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    // Gets a refference of the .on() method in the Eventing class.
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-    this.sync.fetch(id).then((res: AxiosResponse): void => this.set(res.data));
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((res: AxiosResponse): void => this.trigger('save'))
-      .catch(() => this.trigger('error'));
-  }
+export class User extends Model<UserProps> {
+	static buildUser(attrs: UserProps): User {
+		return new User(
+			new Attributes<UserProps>(attrs),
+			new Eventing(),
+			new ApiSync<UserProps>(rootUrl)
+		);
+	}
 }
